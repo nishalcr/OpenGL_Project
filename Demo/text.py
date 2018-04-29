61c9896058c50c69b3e60e338ec87299a3a6288f
PAUL <paulvikiraj@gmail.com>
2018-04-29 15:18:57 +0530

    Able to print the text,check it out in text.py

--- Demo/text.py
+++ Demo/text.py
@@ -1,54 +1,32 @@
-from __future__ import print_function
-from OpenGLContext import testingcontext
-
-BaseContext = testingcontext.getInteractive()
-from OpenGL.GL import *
 from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
+name=('Pratheek T')
 
-def drawText(value, x, y, windowHeight, windowWidth, step=18):
-    """Draw the given text at given 2D position in window
-    """
-    glMatrixMode(GL_PROJECTION);
-    # For some reason the GL_PROJECTION_MATRIX is overflowing with a single push!
-    # glPushMatrix()
-    matrix = glGetDouble(GL_PROJECTION_MATRIX)
+def myInit():
+    glClearColor(1.0,1.0,1.0,1.0)
+    glMatrixMode(GL_PROJECTION)
+    gluOrtho2D(-50,500,-50,500)
 
-    glLoadIdentity();
-    glOrtho(0.0, windowHeight or 32, 0.0, windowWidth or 32, -1.0, 1.0)
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-    glRasterPos2i(x, y);
-    lines = 0
-    ##	import pdb
-    ##	pdb.set_trace()
-    for character in value:
-        if character == '\n':
-            glRasterPos2i(x, y - (lines * 18))
-        else:
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(character));
-    glPopMatrix();
-    glMatrixMode(GL_PROJECTION);
-    # For some reason the GL_PROJECTION_MATRIX is overflowing with a single push!
-    # glPopMatrix();
-    glLoadMatrixd(matrix)  # should have un-decorated alias for this...
+def drawText():
+    glRasterPos2f(0,450)
+    for c in name:
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13, ord(c))
 
-    glMatrixMode(GL_MODELVIEW);
+def display():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(0.0,0.0,0.0)
+    drawText()
+    glFlush()
+    return
 
-
-class TestContext(BaseContext):
-    initialPosition = (0, 0, 0)  # set initial camera position, tutorial does the re-positioning
-
-    def OnInit(self):
-        BaseContext.OnInit(self)
-        print("""Should see "hello world" in white in the lower-left corner of black screen""")
-
-    def Render(self, mode=0):
-        BaseContext.Render(self, mode)
-        glColor3f(0, 0, 1)
-        drawText('hello world', 10, 20, self.viewportDimensions[0], self.viewportDimensions[1])
-
-
-if __name__ == "__main__":
-    TestContext.ContextMainLoop()
+if __name__=='__main__':
+    glutInit(sys.argv)
+    glutInitWindowSize(500,500)
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
+    glutInitWindowPosition(0,0)
+    glutCreateWindow(b'text')
+    myInit()
+    glutDisplayFunc(display)
+    glutMainLoop()
